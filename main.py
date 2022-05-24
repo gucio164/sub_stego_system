407f01af525c3e5cbc5f2e85b24dd6606fbab71a
bgutows1 <b.s.gutowski@stud.elka.pw.edu.pl>
2022-05-24 08:31:59 -0400

    losowosc jest zla

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ import spacy
 import re
 import queue
 import requests
+from bitarray import bitarray
+from itertools import product
+import random
 
 sp = spacy.load('en_core_web_sm')
+stemmer = SnowballStemmer(language="english")
+
+random.seed(9001)
 
 'freq of english alphabet letters '
-alphabet = [(0.1, ' '), (0.082, 'a'),(0.015, 'b'),(0.027, 'c'),(0.043, 'd'),(0.13, 'e'),(0.022, 'f'),(0.021, 'g'),(0.061, 'h'),(0.07, 'i'),
-             (0.0015, 'j'),(0.0077, 'k'),(0.04, 'l'),(0.024, 'm'),(0.067, 'n'),(0.075, 'o'),(0.019, 'p'),(0.00095, 'q'),(0.06, 'r'),
-             (0.063, 's'),(0.091, 't'),(0.028, 'u'),(0.0098, 'v'),(0.024, 'w'),(0.0015, 'x'),(0.02, 'y'),(0.00074, 'z'),]
+alphabet = [(0.1, ' '), (0.082, 'a'), (0.015, 'b'), (0.027, 'c'), (0.043, 'd'), (0.13, 'e'), (0.022, 'f'), (0.021, 'g'),
+            (0.061, 'h'), (0.07, 'i'),
+            (0.0015, 'j'), (0.0077, 'k'), (0.04, 'l'), (0.024, 'm'), (0.067, 'n'), (0.075, 'o'), (0.019, 'p'),
+            (0.00095, 'q'), (0.06, 'r'),
+            (0.063, 's'), (0.091, 't'), (0.028, 'u'), (0.0098, 'v'), (0.024, 'w'), (0.0015, 'x'), (0.02, 'y'),
+            (0.00074, 'z')]
 'Code responsible for creating Huffman tree'
 
 
@@ -53,6 +62,18 @@ def walk_tree(node, prefix="", code={}):
     return (code)
 
 
+def dic_bin_codes_decode(freq):
+    node = create_tree(freq)
+    code = walk_tree(node)
+    dic = {}
+    for i in sorted(freq, reverse=True):
+        try:
+            dic[i[1]] = bitarray(code[i[1]])
+        except:
+            KeyError
+    return dic
+
+
 def dic_bin_codes(freq):
     node = create_tree(freq)
     code = walk_tree(node)
@@ -63,6 +84,8 @@ def dic_bin_codes(freq):
         except:
             KeyError
     return dic
+
+
 'End of Huffman tree section'
 
 
@@ -76,11 +99,22 @@ def hd_msg(message, alph):
 
 def remove_suffix(text):
     res = []
-    stemmer = SnowballStemmer(language="english")
-
     for word in text:
         a = stemmer.stem(word)
         res.append(a)
+    return res
+
+
+def add_suffix(text, orginal):
+    res = []
+    if len(text) == len(orginal):
+        for word, org in zip(text, orginal):
+            a = stemmer.stem(org)
+            if word == a:
+                res.append(org)
+            else:
+                print('')
+
     return res
 
 
@@ -93,24 +127,88 @@ def pos_tag(text):
     return res
 
 
+def decode(message):
+    alp_decode = dic_bin_codes_decode(alphabet)
+    temp = ''.join(message)
+    msg_decode = bitarray(temp).decode(alp_decode)
+    print(''.join(msg_decode))
+
+
+def list_synonyms(word, pos):
+    synonyms = []
+    if pos == 'NOUN':
+        pos = wn.NOUN
+    if pos == 'VERB':
+        pos = wn.VERB
+    for syn in wn.synsets(word, pos):
+        for i in syn.lemmas():
+            if i.name() == word:
+                continue
+            synonyms.append(i.name())
+    return synonyms
+
+
+def check_similarity(word, synon):
+    dic = {}
+    for syn in synon:
+        doc_1 = sp(word)
+        doc_2 = sp(syn)
+        similarity = doc_1.similarity(doc_2)
+        dic.update({syn: similarity})
+    return dic
+
+
+
 if __name__ == "__main__":
-    '''
     'coding alphabet and secret message letters'
-    alp_cl = dic_bin_codes(alphabet)
-    print(alp_cl)
+
+    alp_code = dic_bin_codes(alphabet)
+    #print(alp_code)
     secret = "text to split into sentences"
     secret_list = list(secret)
-    print(secret_list)
-    msg = hd_msg(secret_list, alp_cl)
+    #print(secret_list)
+    msg = hd_msg(secret_list, alp_code)
+    msg = ''.join(msg)
     print(msg)
-    '''
 
     '''
+    print(list_synonyms('love', 'NOUN'))
+    similarity = check_similarity('text', list_synonyms('text', 'NOUN'))
+    similarity = {y: x for x, y in similarity.items()}
+    print(similarity)
+    similarity_code = dic_bin_codes(list(similarity.items()))
+    print(similarity_code)
+    '''
+
     'suffix removal and pos tagging'
     secret = "friday's child is loving and giving"
     tokens = nltk.word_tokenize(secret)
-    suff_less = remove_suffix(tokens)
-    print(suff_less)
-    print(spacy.explain(pos_tag(' '.join(suff_less))))
-    '''
-    print(wn.synset('love.n.02').lemmas()[0].name())
+    suff_less_secret = remove_suffix(tokens)
+    #print(suff_less_secret)
+    #print(pos_tag(' '.join(suff_less_secret)))
+
+    orig_text = open('covert.txt', 'r')
+    words = nltk.word_tokenize(orig_text.read())
+
+    while(1):
+        rand_nr = random.randint(0, len(words))
+        suff_less_word = stemmer.stem(words[rand_nr])
+        word_pos = pos_tag(suff_less_word)
+        if word_pos[0] == 'NOUN':
+            print("")
+            similarity = check_similarity(suff_less_word, list_synonyms(suff_less_word, 'NOUN'))
+            similarity = {y: x for x, y in similarity.items()}
+            similarity_code = dic_bin_codes(list(similarity.items()))
+            similarity_code = {y: x for x, y in similarity_code.items()}
+            print(similarity_code)
+            break
+        elif word_pos[0] == 'VERB':
+            print("")
+            similarity = check_similarity(suff_less_word, list_synonyms(suff_less_word, 'VERB'))
+            similarity = {y: x for x, y in similarity.items()}
+            similarity_code = dic_bin_codes(list(similarity.items()))
+            similarity_code = {y: x for x, y in similarity_code.items()}
+            print(similarity_code)
+            break
+        else:
+            print("")
